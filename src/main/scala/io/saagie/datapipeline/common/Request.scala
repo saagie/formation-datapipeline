3903d6fddbfd17177ef429be2b612008ee7519be
Erwan KOFFI <erwan@saagie.com>
2017-10-05 14:43:02 +0200

    + with latitude and longitude

--- src/main/scala/io/saagie/datapipeline/common/Request.scala
+++ src/main/scala/io/saagie/datapipeline/common/Request.scala
@@ -3,13 +3,13 @@ package io.saagie.datapipeline.common
 import java.time.Instant
 import java.util.UUID
 
-import fabricator.{Fabricator, Internet, UserAgent}
+import fabricator.{Fabricator, Internet, Location, UserAgent}
 import org.joda.time.DateTimeZone
 
 import scala.collection.mutable
 import scala.util.Random
 
-case class Request(uuid: UUID, origin: String, ip: String, userAgent: Option[String], timestamp: Long) {
+case class Request(uuid: UUID, origin: String, ip: String, userAgent: Option[String], timestamp: Long, latitude: Double, longitude: Double) {
   /**
     * Trick to transform UUID as String.
     * Used as a workaround Spark's serialization.
@@ -22,17 +22,22 @@ case class Request(uuid: UUID, origin: String, ip: String, userAgent: Option[Str
 object Request {
   DateTimeZone.setDefault(DateTimeZone.UTC)
 
-  def apply(): Request = new Request(UUID.randomUUID(), "", "", None, 0L)
+  def apply(): Request = new Request(UUID.randomUUID(), "", "", None, 0L, 0, 0)
 
   val userAgent: UserAgent = Fabricator.userAgent()
   val internet: Internet = Fabricator.internet()
-  val columnsNames = Seq("uuid", "origin", "ip", "user_agent", "timestamp")
+  val location: Location = Fabricator.location()
+  val columnsNames = Seq("uuid", "origin", "ip", "user_agent", "timestamp", "latitude", "longitude")
 
   def generateRequest(): Request = {
     new Request(UUID.randomUUID(),
       internet.urlBuilder.host("formation.test").params(mutable.Map("q" -> s"${Random.nextInt(32)}")).toString,
       internet.ip,
       if (Random.nextBoolean) Some(userAgent.browser) else None,
-      Instant.now.getEpochSecond)
+      Instant.now.getEpochSecond,
+      location.latitude(-89, 89).toDouble,
+      location.longitude(-179, 179).toDouble)
+
+    location.coordinates
   }
 }
